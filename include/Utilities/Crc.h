ba71b36473d2157d9028a1a4df829282b5e74d2b
Simon <simon.hobbs@electrooptical.net>
2020-05-13 18:09:45 -0400

    adding tests

--- include/Utilities/Crc.h
+++ include/Utilities/Crc.h
@@ -59,11 +59,11 @@ inline constexpr uint8_t crc_uint8(const uint8_t *const buffer,
   for (std::size_t current_byte = 0; current_byte < data_length; current_byte++) {
     crc ^= (buffer[current_byte]);
     for (std::size_t crc_bit = 8; crc_bit > 0; --crc_bit) {
+      const auto crc_shift = static_cast<uint8_t>(crc << 1);
       if (crc & 0x80) {
-        const auto crc_shift = static_cast<uint8_t>(crc << 1);
         crc = (crc_shift) ^ crc_polynomial;
       } else {
-        crc <<= 1;
+        crc = crc_shift;
       }
     }
   }
@@ -77,8 +77,8 @@ inline constexpr uint8_t crc8(const uint8_t *const buffer,
   return crc_uint8(buffer, data_length, crc8_polynomial, crc_initial_value);
 }
 
-static_assert(crc8(std::array<uint8_t, 2>{0xBE, 0xEF}.data(), 2) == 0x92);
-static_assert(crc8(std::array<uint8_t, 1>{0x00}.data(), 1) == 0xAC);
+static_assert(crc8(std::array<uint8_t, 2>{0xBE, 0xEF}.data(), 2, 0xff) == 0x92);
+static_assert(crc8(std::array<uint8_t, 1>{0x00}.data(), 1, 0xff) == 0xAC);
 
 
 inline constexpr uint8_t crc4_new(const uint8_t *const buffer,
@@ -86,27 +86,26 @@ inline constexpr uint8_t crc4_new(const uint8_t *const buffer,
                               const uint8_t crc_initial_value = 0) {
   const constexpr uint8_t crc4_polynomial = 0x03;
   const uint8_t crc = crc_uint8(buffer, data_length, crc4_polynomial, crc_initial_value);
-  const uint8_t last4_bits = (0x000F & (crc >> 4)); // final 4-bit reminder is CRC code
-  return 0xf & (last4_bits ^ 0x0);
+  return crc & 0xf;
 }
 
 inline constexpr uint8_t crc4(const uint8_t *const buffer,
                               const std::size_t data_length) {
   const constexpr uint16_t crc4_polynomial = 0x3000;
-  uint16_t n_rem = 0; //  crc reminder
+  uint16_t crc = 0; //  crc reminder
 
   for (std::size_t current_byte = 0; current_byte < data_length; current_byte++) {
-    n_rem ^= buffer[current_byte];
-    for (std::size_t n_bit = 8; n_bit > 0; n_bit--) {
-      if (n_rem & (0x8000)) {
-        n_rem = (n_rem << 1) ^ crc4_polynomial;
+    crc = static_cast<uint16_t>(crc ^ buffer[current_byte]);
+    for (int n_bit = 8; n_bit > 0; n_bit--) {
+      const auto crc_shift = static_cast<uint16_t>(crc << 1);
+      if (crc & (0x8000)) {
+        crc = crc_shift ^ crc4_polynomial;
       } else {
-        n_rem = (n_rem << 1);
+        crc = crc_shift;
       }
     }
   }
-  n_rem = (0x000F & (n_rem >> 12)); // final 4-bit reminder is CRC code
-  return 0xf & (n_rem ^ 0x0);
+  return (0x000F & (crc >> 12)); // take upper 4 bits 
 }
 } //  namespace Utilities
 
