b9bf094e947a2fb63be52e3448f77afae747bddf
Simon <simon.hobbs@electrooptical.net>
2020-05-19 18:08:14 -0400

    adding at

--- include/ArrayView/ArrayView.h
+++ include/ArrayView/ArrayView.h
@@ -6,6 +6,18 @@
 #ifndef ARRAYVIEW_ARRAYVIEW_H_
 #define ARRAYVIEW_ARRAYVIEW_H_
 #include <cassert>
+
+#if 0
+#include <string_view>
+template<typename T>
+class ArrayView : public std::basic_string_view<T>{
+public:
+  ArrayView(std::size_t length, T* pointer) :
+    std::basic_string_view<T>{length, pointer} {
+      assert(length >= 0);
+  }
+};
+#else
 template<typename T>
 class ArrayView {
     const std::size_t length_;
@@ -15,6 +27,10 @@ class ArrayView {
     [[deprecated]]
     std::size_t Size(void) const { return length_; }
     std::size_t size(void) const { return length_; }
+    T& at(std::size_t index) {
+        assert(index <= size());
+        return pointer_[index < size() ? index : 0];
+    }
     const T& at(std::size_t index) const {
         assert(index <= size());
         return pointer_[index < size() ? index : 0];
@@ -50,5 +66,6 @@ class ArrayView {
             assert(length >= 0);
         }
 };
+#endif
 
 #endif  //  ARRAYVIEW_ARRAYVIEW_H_
