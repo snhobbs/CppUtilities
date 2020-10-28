7864033689215555c40c2d063d49fac3ae5f1a88
Simon <simon.hobbs@electrooptical.net>
2020-10-27 21:45:29 -0400

    added conversion of array type to bytes

--- include/Utilities/TypeConversion.h
+++ include/Utilities/TypeConversion.h
@@ -267,5 +267,17 @@ inline constexpr T StaticCastQuickFail(U x) {
   return static_cast<T>(x);
 }
 
+template<typename T>
+void ArrayToBytes(const T * const data_in, uint8_t * const data_out, const std::size_t size_in, const std::size_t size_out) {
+  const std::size_t dsize_mult = size_out/sizeof(T);
+  std::size_t data_out_count = 0;
+  for (std::size_t i = 0; i < std::min(size_in, size_out/dsize_mult); i++) {
+    for (std::size_t j = 0; j < dsize_mult; j++) {
+      data_out[i*dsize_mult + j] = 0xff & (data_in[i] >> dsize_mult*j);
+    }
+  }
+}
+
+
 }  //  namespace Utilities
 #endif  //  UTILITIES_TYPECONVERSION_H_
