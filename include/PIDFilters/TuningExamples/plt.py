02fd493c44f7951e4acebf852bd31ef3caba8f4e
Simon <simon.hobbs@electrooptical.net>
2021-01-09 10:51:33 -0500

    removing tuning code

--- include/PIDFilters/TuningExamples/plt.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as plt
-import numpy as np
-
-time, data = np.loadtxt("BumpTest_2019-12-19T15:18:43_998219.csv", skiprows=6, unpack=True, delimiter=',')
-
-plt.plot(time, data)
-plt.show()
